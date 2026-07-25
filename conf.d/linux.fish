20c1d3875970d3ebb0c4ec031f7c9ca9031b91b6
vasconcj <jotamudo@riseup.net>
2026-07-25 19:46:26 +0200

    feat: add warning if keychain is not installed

--- conf.d/linux.fish
+++ conf.d/linux.fish
@@ -2,4 +2,8 @@ if test "$(uname)" != Linux
   return
 end
 
-echo 'hellooooooooooooooo'
+if type -q $keychain
+  keychain --eval ~/.ssh/id_ed25519 | source
+else
+  echo 'Keychain is not installed, you\'ll have to type your ssh password...'
+end
