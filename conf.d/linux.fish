d1559437dac96202a36bb01b8a4d5da72918b8c9
vasconcj <jotamudo@riseup.net>
2026-07-26 21:16:28 +0200

    fix: check if keychain is installed before lauching it

--- conf.d/linux.fish
+++ conf.d/linux.fish
@@ -2,8 +2,8 @@ if test "$(uname)" != Linux
   return
 end
 
-if type -q $keychain
-  keychain --eval ~/.ssh/id_ed25519 | source
+if command -q keychain
+  keychain -q --eval ~/.ssh/id_ed25519 | source
 else
   echo 'Keychain is not installed, you\'ll have to type your ssh password...'
 end
